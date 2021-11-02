d82543d8b0b7a1b6a74cdde69888b0fd7e757461
jacksplwxy <895685537@qq.com>
2021-11-02 14:28:55 +0800

    一个基础的webview

---
+++ webview/app/src/main/java/com/jacksplwxy/webview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.jacksplwxy.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webViewInit();
+    }
+
+    private void webViewInit() {
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("https://www.baidu.com");
+        //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //使用WebView加载显示url
+                view.loadUrl(url);
+                //返回true
+                return true;
+            }
+        });
+    }
+}
